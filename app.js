cc883ae3c3d7a15997d3f9735dd8b0bed496dac5
Lexirodrguez <rodriguezmatheuslexc@uvm.edu.ve>
2025-10-19 23:25:22 -0400

    cambios temporales

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql2/promise');
 
+// Configuración de la base de datos
 const dbConfig = {
   host: 'localhost',
   user: 'root',
@@ -12,45 +13,51 @@ const dbConfig = {
   database: 'cinepolis',
 };
 
-var peliculasRouter = require('./routes/Peliculas')(mysql, dbConfig);
-var funcionesRouter = require('./routes/funciones')(mysql, dbConfig);
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 var app = express();
 
-
+// Configuración de vistas
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Peliculas', (req, res) => {
-  res.render("Peliculas");
-});
-app.use('/funciones', (req, res) => {
-  res.render('funciones');
-});
-app.use('/api/Peliculas', peliculasRouter); 
-app.use('/api/funciones', funcionesRouter); 
+// Rutas principales
 
-// catch 404 and forward to error handler
+// Ruta raíz
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+// Ruta para películas
+app.get('/peliculas', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'peliculas.html'));
+});
+
+// Ruta para funciones
+app.get('/funciones', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'funciones.html'));
+});
+
+// Manejo de errores 404
 app.use(function(req, res, next) {
-  next(createError(404));
+  res.status(404).send(`
+    <h1>404 - Página no encontrada</h1>
+    <p>La página que buscas no existe.</p>
+  `);
 });
 
-// error handler
+// Manejo de otros errores
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
